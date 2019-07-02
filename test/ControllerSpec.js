7c28d5e898ddc02b08f7819a821e0c843e73ddf5
BataLaSalade <dsfmarina@gmail.com>
2019-07-02 15:02:45 +0200

    TU - Controller should highlight Active filter

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -163,6 +163,12 @@ describe('controller', function () {
 
 	it('should highlight "Active" filter when switching to active view', function () {
 		// TODO: write test
+		var todo = {id: 42, title: 'my todo', completed: true};
+		setUpModel([todo]);
+
+		subject.setView('#/active');
+
+		expect(view.render).toHaveBeenCalledWith('setFilter', "active");
 	});
 
 	describe('toggle all', function () {
